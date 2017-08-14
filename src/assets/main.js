b8e71252cc0720123ff7765767dad0f63d0e30ab
Micheal Ashley <micheal.ashley@macefusion.com>
2017-08-14 07:42:49 -0400

    Fixes the class name in the div

--- src/assets/main.js
+++ src/assets/main.js
@@ -9,7 +9,7 @@ $(function() {
 			console.log(response.courses.completed);
 			$.each(response.courses.completed, function(key, value){
 				console.log(value.title);
-				$('#badges').append('<div class="courses"><h3>'+value.title+'</h3>'
+				$('#badges').append('<div class="course"><h3>'+value.title+'</h3>'
 				+ '<img src="'+value.badge+'"/><a href="'+value.url+'" target=_blank class="btn btn-primary">See Course</a></div>');
 			});
 		}
