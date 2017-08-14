89522f8a23ef2973cf203744d638cadfbad7b09e
Micheal Ashley <micheal.ashley@macefusion.com>
2017-08-14 07:40:43 -0400

    Adds my CodeSchool badges

--- src/assets/main.js
+++ src/assets/main.js
@@ -2,4 +2,17 @@ $(function() {
 
   // your code will go here
 
+	$.ajax({
+		url: 'https://www.codeschool.com/users/mashley82.json',
+		dataType: 'jsonp',
+		success: function(response) {
+			console.log(response.courses.completed);
+			$.each(response.courses.completed, function(key, value){
+				console.log(value.title);
+				$('#badges').append('<div class="courses"><h3>'+value.title+'</h3>'
+				+ '<img src="'+value.badge+'"/><a href="'+value.url+'" target=_blank class="btn btn-primary">See Course</a></div>');
+			});
+		}
+	});
+
 });
